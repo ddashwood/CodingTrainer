b89e18089fc78174d1c8f3828ea6216b94c9401c
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2018-01-27 21:17:41 +0000

    Move functions out of the global namespace

--- CodingTrainerWeb/Scripts/app/RunCode.js
+++ CodingTrainerWeb/Scripts/app/RunCode.js
@@ -1,51 +1,31 @@
-﻿$(function () {
-    // Default data
-    $('#code').val(model.DefaultCode);
+﻿var app = {};
 
-    // Set up and maintain the connection
-    $.connection.hub.start().done(connected);
-    $.connection.hub.disconnected(disconnected);
-
-    // Set up the hub client callbacks
-    var hub = $.connection.codeRunnerHub;
-    hub.client.consoleOut = consoleOut;
-    hub.client.complete = complete;
-
-    // Set up JQuery event handlers
-    $('#run').click(run);
-    $('#console').click(consoleIn);
-
-
-
-
+(function (exports) {
     // Code for starting/maintaining SignalR connection
 
-    function connected() {
+    exports.connected = function () {
         // Allow users to submit code
         $('#run').prop('disabled', false).text('Run Code');
-    }
+    };
 
-    function disconnected() {
+    exports.disconnected = function () {
         // Attempt to reconnect
         $.connection.hub.start();
-    }
-
-
+    };
 
     // Code for handling SignalR events
 
-    function consoleOut(message) {
+    exports.consoleOut = function (message) {
         $('#console-out').append(document.createTextNode(message));
-    }
+    };
 
-    function complete() {
+    exports.complete = function () {
         $('#run').prop('disabled', false);
-    }
-
+    };
 
     // Code for handling window events
 
-    function run() {
+    exports.run = function () {
         $('#run').prop('disabled', true);
         $('#console-out').text('');
         try {
@@ -61,9 +41,29 @@
             alert(e.message);
             complete();
         }
-    }
+    };
 
-    function consoleIn() {
+    exports.consoleIn = function () {
         hub.server.consoleIn($('#console-in').val());
-    }
-});
+    };
+})(app);
+
+
+
+
+// Code that runs on startup:
+
+$('#code').val(model.DefaultCode);
+
+// Set up and maintain the connection
+$.connection.hub.start().done(app.connected);
+$.connection.hub.disconnected(app.disconnected);
+
+// Set up the hub client callbacks
+var hub = $.connection.codeRunnerHub;
+hub.client.consoleOut = app.consoleOut;
+hub.client.complete = app.complete;
+
+// Set up JQuery event handlers
+$('#run').click(app.run);
+$('#console').click(app.consoleIn);
