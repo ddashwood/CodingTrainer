3a4e321090a557d7284a0ec25dc1b77f1b87366c
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2018-12-21 20:36:53 +0000

    Fixed configuration so it works on IIS Express as well as on Azure.
    Course material implemented with partial views instead of with Ajax

---
+++ CodingTrainerWeb/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodingTrainer.CodingTrainerWeb.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Content(int chapter, int exercise)
+        {
+            string viewName = $"c{chapter}_e{exercise}";
+            ViewEngineResult result = ViewEngines.Engines.FindView(ControllerContext, viewName, null);
+
+            if (result.View == null)
+            {
+                return PartialView("NotFound");
+            }
+
+            return PartialView(viewName);
+        }
+    }
+}
