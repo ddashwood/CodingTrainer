bc7d03baf75657d5ba0dd2078ff6008a3534c05a
Dean Dashwood <dean.dashwood@yahoo.co.uk>
2019-01-08 10:51:36 +0000

    Improved testing

---
+++ AssessmentTests/PreProgrammedTextReaderTests.cs
@@ -0,0 +1,39 @@
+﻿using CodingTrainer.CSharpRunner.Assessment;
+using CodingTrainer.CSharpRunner.TestingCommon;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTrainer.CSharpRunner.AssessmentTests
+{
+    class PreProgrammedTextReaderTests: CodeHostTestBase
+    {
+        [Test]
+        [Category("Assessment")]
+        public async Task ConsoleInTest()
+        {
+            var compiled = await runner.Compile(GetUsings(new string[] { "System" }).
+                Append(WrapInMain("Console.WriteLine(\"You entered \" + Console.ReadLine()); " +
+                                  "Console.WriteLine(\"You entered \" + Console.ReadLine());")).ToString());
+
+            await runner.Run(compiled, new PreProgrammedTextReader("Test1\r\nTest2\r\n"));
+
+            Assert.AreEqual("Test1\r\nYou entered Test1\r\nTest2\r\nYou entered Test2\r\n", console.ToString());
+        }
+
+        [Test]
+        [Category("Assessment")]
+        public async Task ReadTooMuchTest()
+        {
+            var compiled = await runner.Compile(GetUsings(new string[] { "System" }).
+                Append(WrapInMain("Console.ReadLine(); Console.ReadLine();")).ToString());
+
+            var e = Assert.ThrowsAsync<AggregateException>(() => runner.Run(compiled, new PreProgrammedTextReader("Test1\r\n")));
+            Assert.That(e.InnerException, Is.TypeOf<EndOfStreamException>());
+        }
+    }
+}
